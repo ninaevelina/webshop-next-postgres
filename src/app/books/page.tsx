00edba08e4f83eed1905fc2b61b03e03e4bc662d
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-10-03 14:52:33 +0200

    adjusts books component to include search functionality and adjusts params for page /books

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,10 +1,17 @@
+import { fetchFilteredBooks } from "../lib/data";
 import Books from "../ui/books/books";
 
-export default async function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string };
+}) {
+  const query = searchParams?.query || "";
+  const books = await fetchFilteredBooks(query);
   return (
     <main>
       <section className="container">
-        <Books />
+        <Books books={books} />
       </section>
     </main>
   );
--- src/app/ui/books/books.tsx
+++ src/app/ui/books/books.tsx
@@ -1,38 +1,39 @@
-import { fetchBooks } from "@/app/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 import "./books.scss";
+import Search from "../search/search";
+import { Book } from "@/app/lib/definitions";
 
-export default async function Books() {
-  const books = await fetchBooks();
-  console.log(books);
-
+export default async function Books({ books }: { books: Book[] }) {
   return (
-    <ul className="booklist">
-      {books.map((book) => {
-        return (
-          <li key={book.id} className="list-item">
-            <article className="book-card">
-              <div>
-                <Image
-                  src={book.image_url}
-                  alt={`Image of ${book.name}`}
-                  height={220}
-                  width={180}
-                />
-              </div>
-              <div className="book-card__details">
-                <Link href={`/books/${book.id}`}>{book.name}</Link>
-                <p>{book.author_name}</p>
-                <p>{book.price}:-</p>
-                <button className="book-card__details--button">
-                  Add to cart
-                </button>
-              </div>
-            </article>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <Search placeholder="Search for books" />
+      <ul className="booklist">
+        {books.map((book) => {
+          return (
+            <li key={book.id} className="list-item">
+              <article className="book-card">
+                <div>
+                  <Image
+                    src={book.image_url}
+                    alt={`Image of ${book.name}`}
+                    height={220}
+                    width={180}
+                  />
+                </div>
+                <div className="book-card__details">
+                  <Link href={`/books/${book.id}`}>{book.name}</Link>
+                  <p>{book.author_name}</p>
+                  <p>{book.price}:-</p>
+                  <button className="book-card__details--button">
+                    Add to cart
+                  </button>
+                </div>
+              </article>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
